bc2c08867aa7844cfd3f3b9124f67f8e43700a32
shanmugamlevon <shanmugam070702@gmail.com>
2026-01-30 03:27:49 +0530

    axiosconfig changed

--- frontend/src/Axiosconfig.jsx
+++ frontend/src/Axiosconfig.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const Instance = axios.create({
-    // baseURL:"https://schoolmanagement-api-39gd.onrender.com/" //render
-    baseURL:"http://localhost:5000/" //local
+    baseURL:"https://schoolmanagement-api-39gd.onrender.com/" //render
+    // baseURL:"http://localhost:5000/" //local
 })
 
 export default Instance;
